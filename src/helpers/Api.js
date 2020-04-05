fcb201d9a2ea17baaf8e3b8e75d3da6c6ed8dea6
Bart Willems <bwillems@protonmail.com>
2020-04-06 00:00:55 +0200

    attempt to remove the user's session when 401 Unauthorized is returned from the server
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

--- src/helpers/Api.js
+++ src/helpers/Api.js
@@ -1,9 +1,23 @@
 import Axios from 'axios';
+import { removeSession } from './Session';
 
 const ApiClient = Axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   timeout: 1000,
   withCredentials: true,
 });
+
+ApiClient.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    // I don't know yet if this is a good idea
+    if (error.response?.status === 401) {
+      removeSession();
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default ApiClient;
