023812c4389e13dd69e4294d7c7c7c703a24330d
Bart Willems <bwillems@protonmail.com>
2020-09-30 22:03:39 +0200

    temporarily set the footer to relative because it's present over some content on mobile
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ const App = () => {
           <Footer
             style={{
               textAlign: 'center',
-              position: 'fixed',
+              position: 'relative',
               width: '100%',
               bottom: '0',
               left: '0',
