ca3c4986927b83bdd4cd34fa4da03d12a64c1d27
Bart Willems <bwillems@protonmail.com>
2020-10-06 22:49:14 +0200

    added a home icon to the navbar
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -5,12 +5,17 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import HomeIcon from "@material-ui/icons/Home";
 
 import UserMenu from "./UserMenu";
 
 const useStyles = makeStyles((theme) => ({
   spacer: {
     flexGrow: 1,
+  },
+  homeIcon: {
+    marginRight: theme.spacing(1),
+    float: "left",
   },
 }));
 
@@ -22,14 +27,15 @@ const NavBar = () => {
     <AppBar position="static">
       <Container style={{ padding: "0" }}>
         <Toolbar>
-          <Typography
+          <div
+            style={{ display: "flex", alignItems: "center", cursor: "pointer" }}
             onClick={() => history.push("/")}
-            variant="h6"
-            style={{ cursor: "pointer" }}
           >
-            Beursfuif
-          </Typography>
-          {/* Span is used as a spacer */}
+            <HomeIcon className={classes.homeIcon} />
+            <Typography variant="h6" style={{ float: "left" }}>
+              Beursfuif
+            </Typography>
+          </div>
           <span className={classes.spacer} />
           <UserMenu />
         </Toolbar>
