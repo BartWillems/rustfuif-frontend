051ffd32a4c88941e52f3d18e7218c67b9eb7e92
Bart Willems <bwillems@protonmail.com>
2020-10-20 16:21:21 +0200

    added sentry
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import { AuthenticationProvider } from "./global";
 import "./index.css";
 import App from "./App";
+
+Sentry.init({
+  dsn:
+    "https://836635c7c619407f8ee19abd3eed06fb@o464327.ingest.sentry.io/5472355",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 0.5,
+});
 
 ReactDOM.render(
   <AuthenticationProvider>
